f81679c3915167eaf47769a284498de475eb6f27
あゆみ(Ayumi) きよみず(KIyomizu) <kiyo.a@outlook.com>
2025-07-21 20:44:08 -0400

    OOP Mastercalss Challenge
    Restaurant app with deluxe code

---
+++ Drink.java
@@ -0,0 +1,27 @@
+class Drink extends Restaurant{
+    private String type;
+    private DrinkSize size;
+    private double basePrice;
+
+    public Drink(String type, DrinkSize size) {
+        this.type = type;
+        this.size = size;
+        this.basePrice = size.getPrice();   // Set base price according to initial size
+    }
+    public String getType() {
+        return type;
+    }
+
+    public DrinkSize getSize() {
+        return size;
+    }
+
+    public void setSize(DrinkSize newSize) {
+        this.size = newSize;
+        this.basePrice = newSize.getPrice();    //Update price when size changes
+    }
+
+    public double getPrice() {
+        return basePrice;
+    }
+}
